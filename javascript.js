9e08c11cc25889df526370f823cf9339970be06f
icomedy <43688844+icomedy@users.noreply.github.com>
2019-02-03 13:48:18 -0500

    lol

--- javascript.js
+++ javascript.js
@@ -1,18 +1,10 @@
-var NumberOfWords = 9
+var NumberOfWords = 1
 
 var words = new BuildArray(NumberOfWords)
 
 // Use the following variables to 
 // define your random words:
-words[1] = "http://shortearn.eu/NBkNi"
-words[2] = "http://shortearn.eu/qAxZUAgH"
-words[3] = "http://shortearn.eu/AXAg"
-words[4] = "http://shortearn.eu/wxv5PKYk"
-words[5] = "http://shortearn.eu/ZL5f0VR"
-words[6] = "http://shortearn.eu/2FEJ"
-words[7] = "http://shortearn.eu/5eOzLz"
-words[8] = "http://shortearn.eu/eVSt"
-words[9] = "http://shortearn.eu/kbzNtpmm"
+words[1] = "Oops! There are no Fortnite accounts instock! Comeback later!"
 
 function BuildArray(size){
 this.length = size
